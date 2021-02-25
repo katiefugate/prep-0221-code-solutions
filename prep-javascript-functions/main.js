ce240970bee8543017f343d3422f6e5a1392b68c
katiefugate <katiiierochelle@yahoo.com>
2021-02-25 15:29:01 -0800

    completed prep-javascript-functions.

---
+++ prep-javascript-functions/main.js
@@ -0,0 +1,53 @@
+function addTwoNumbers(num1, num2) {
+  return num1 + num2;
+}
+
+console.log(addTwoNumbers(4, 4));
+
+function convertHoursToMinutes(num) {
+  return num * 60;
+}
+
+console.log(convertHoursToMinutes(2));
+
+function personalizeGreeting(name) {
+  return 'Hello ' + name + '!';
+}
+
+console.log(personalizeGreeting('Tim'));
+
+function addAndMultiplyBy5(num1, num2) {
+  return (num1 + num2) * 5;
+
+}
+
+console.log(addAndMultiplyBy5(10, 5));
+
+function multiplyAndDivideBy5(num1, num2) {
+  return num1 * num2 / 5;
+}
+
+console.log(multiplyAndDivideBy5(35, 10));
+
+function substractTwoNumbers(num1, num2) {
+  return num1 - num2;
+}
+
+console.log(substractTwoNumbers(22, 7));
+
+function getCircleCircumfrence(radius) {
+  return radius * 2 * Math.PI;
+}
+
+console.log(getCircleCircumfrence(5));
+
+function getFullName(firstName, lastName) {
+  return firstName + ' ' + lastName;
+}
+
+console.log(getFullName('Juan', 'Ramirez'));
+
+function cube(number) {
+  return number * number * number;
+}
+console.log(cube(5));
